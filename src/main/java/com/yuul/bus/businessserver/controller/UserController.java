fca55dc08d93ec15969e9d77677cfb394d729730
v-wuyulong <wuyulong@beyondsoft.com>
2022-10-11 15:13:55 +0800

    集成swagger-ui配置

--- src/main/java/com/yuul/bus/businessserver/controller/UserController.java
+++ src/main/java/com/yuul/bus/businessserver/controller/UserController.java
@@ -3,6 +3,9 @@ package com.yuul.bus.businessserver.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.yuul.bus.businessserver.dao.UserDao;
 import com.yuul.bus.businessserver.model.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +15,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("user")
+@Api(tags = "userController",description = "用户接口文档")
 public class UserController {
 
     @Autowired
     UserDao userDao;
 
+    @ApiOperation(value = "司机取消订单",httpMethod = "POST")
     @RequestMapping("/getMsg")
     public Object getUser(){
         QueryWrapper<User> wrapper = new QueryWrapper<>();
